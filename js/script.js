cb133260e6782c3432f4bf6f9004a71145f9f45f
Chloe Havel <113055595+Chloe-Havel@users.noreply.github.com>
2023-01-22 10:47:30 -0500

    added stuff

--- js/script.js
+++ js/script.js
@@ -4,20 +4,34 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
+
 "use strict"
 
-/**
- * Check servie worker.
- */
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test-1/sw.js", {
-    scope: "/ICS2O-PWA-Test-1/",
-  })
-}
 
 /**
- * This function displays an alert.
- */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+* Check servie worker.
+*/
+if (navigator.serviceWorker) {
+ navigator.serviceWorker.register("/ICS2O-assinments-6/sw.js", {
+   scope: "/ICS2O-assinments-6/",
+ })
 }
+
+
+const quote = async (URLAddress) => {
+ try {
+   const result = await fetch(URLAddress)
+   const data = await result.json()
+   console.log(data)
+   document.getElementById("dog").innerHTML =
+   '<h4> dogs:0: </h4> <img src="' + data.message + '" alt="dog" />'
+ } catch (error) {
+   console.log(error)
+ }
+}
+
+
+quote(
+ "https://dog.ceo/api/breeds/image/random"
+)
+
